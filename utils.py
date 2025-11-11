1d3b001a969e9c319ec5d24d3c70167c7aff2b49
Magdalena Gielazyn <magdalenagielazyn22@gmail.com>
2025-11-11 21:38:08 +0100

    next version - eda

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import kagglehub
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+
+
 def rename_columns_snake_case(df):
     dataframe = df.copy()
     dataframe.columns = [col.lower().replace(" ", "_").replace("(", "_").replace(")", "")  for col in dataframe.columns]
@@ -43,3 +48,41 @@ def unique_values(df, num_of_unique_values):
             unique_values = sorted(df[col].unique())
             if len(unique_values) <= num_of_unique_values:
                 print(f"{col}: {df[col].dtype}, {unique_values}\n")
+
+def generate_correlation_matrix(df, method, title):
+    corr = df.corr(numeric_only=True, method=method)
+    plt.figure(figsize=(15, 10))
+    sns.heatmap(corr, 
+    annot=True, 
+    fmt=".2f",
+    cmap="coolwarm",
+    vmin=-1, vmax=1,
+    square=True,
+    linewidths=0.5, 
+    cbar_kws={"shrink": .75})  
+    plt.title(title, fontsize=30)
+    plt.tight_layout()
+    plt.show()
+
+def bmi_calculation(mass, height):
+    bmi = mass / (height/100) ** 2
+    return int(round(bmi))
+
+def pca_method(df, n_components, feature_names, new_feature_name):
+    pca = PCA(n_components=n_components)
+    df[new_feature_name] = pca.fit_transform(df[feature_names])
+    df = df.drop(feature_names, axis=1)
+    return df
+
+def check_target_balance(df, target):
+    df_percentage = df[target].value_counts(normalize=True) * 100
+    num = df_percentage.max()
+    if num <= 60:
+        descr= "Dane są dobrze zrównoważone."
+    elif num <= 70:
+        descr= "Lekka nierównowaga – akceptowalna."
+    elif num <= 80:
+        descr= "Umiarkowana nierównowaga – warto monitorować."
+    else:
+        descr= "Silna nierównowaga – zalecane balansowanie danych."
+    return descr
